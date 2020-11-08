12817cf2a99b255dc08ae7121cda29240fa7c758
kano-kenji <aleksandrtarasevich77@gmail.com>
2020-11-08 14:11:56 -0700

    Fix routing annotation

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 use App\Entity\User;
 use App\Repository\UserRepository;
+use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
